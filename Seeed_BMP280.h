7233e9b2e26366803c266252e64affb819cbbaa5
Baozhu Zuo <zuobaozhu@gmail.com>
2020-02-02 12:41:10 +0800

    Pretty printed the Arduino code with astyle

--- Seeed_BMP280.h
+++ Seeed_BMP280.h
@@ -29,39 +29,38 @@
 #define BMP280_REG_PRESSUREDATA    0xF7
 #define BMP280_REG_TEMPDATA        0xFA
 
-class BMP280
-{
-public:  
-  bool init(int i2c_addr = BMP280_ADDRESS);
-  float getTemperature(void);
-  uint32_t getPressure(void);
-  float calcAltitude(float pressure);
-private:
-  bool isTransport_OK;
-  int _devAddr;
-  // Calibratino data
-  uint16_t dig_T1;
-  int16_t dig_T2;
-  int16_t dig_T3;
-  uint16_t dig_P1;
-  int16_t dig_P2;
-  int16_t dig_P3;
-  int16_t dig_P4;
-  int16_t dig_P5;
-  int16_t dig_P6;
-  int16_t dig_P7;
-  int16_t dig_P8;
-  int16_t dig_P9;
-  int32_t t_fine;
+class BMP280 {
+  public:
+    bool init(int i2c_addr = BMP280_ADDRESS);
+    float getTemperature(void);
+    uint32_t getPressure(void);
+    float calcAltitude(float pressure);
+  private:
+    bool isTransport_OK;
+    int _devAddr;
+    // Calibratino data
+    uint16_t dig_T1;
+    int16_t dig_T2;
+    int16_t dig_T3;
+    uint16_t dig_P1;
+    int16_t dig_P2;
+    int16_t dig_P3;
+    int16_t dig_P4;
+    int16_t dig_P5;
+    int16_t dig_P6;
+    int16_t dig_P7;
+    int16_t dig_P8;
+    int16_t dig_P9;
+    int32_t t_fine;
 
-  // private functoins
-  uint8_t bmp280Read8(uint8_t reg);
-  uint16_t bmp280Read16(uint8_t reg);
-  uint16_t bmp280Read16LE(uint8_t reg);
-  int16_t bmp280ReadS16(uint8_t reg);
-  int16_t bmp280ReadS16LE(uint8_t reg);
-  uint32_t bmp280Read24(uint8_t reg);
-  void writeRegister(uint8_t reg, uint8_t val);
+    // private functoins
+    uint8_t bmp280Read8(uint8_t reg);
+    uint16_t bmp280Read16(uint8_t reg);
+    uint16_t bmp280Read16LE(uint8_t reg);
+    int16_t bmp280ReadS16(uint8_t reg);
+    int16_t bmp280ReadS16LE(uint8_t reg);
+    uint32_t bmp280Read24(uint8_t reg);
+    void writeRegister(uint8_t reg, uint8_t val);
 };
 
 #endif
